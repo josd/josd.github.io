8cd491150a85b0e23ad7452bd9d214f2b1ba0ad5
josd <josderoo@gmail.com>
2022-01-15 14:54:11 +0100

    partial webizing

--- temp/gendt.pl
+++ temp/gendt.pl
@@ -1,25 +1,27 @@
 % Generating deep taxonomy
 
+:- use_module(library(between)).
+:- use_module(library(format)).
+
 run :-
     open('dt.pl',write,Out),
     write(Out,'% Deep taxonomy\n'),
     write(Out,'% See http://ruleml.org/WellnessRules/files/WellnessRulesN3-2009-11-10.pdf\n'),
     write(Out,'\n'),
-    write(Out,'n0(z).\n'),
+    write(Out,'\'http://example.org/etc#n0\'(\'http://example.org/etc#z\').\n'),
     write(Out,'\n'),
-    (   between(0,9999,I),
+    (   between(0,999,I),
         J is I+1,
-        format(Out,"n~d(X) :- n~d(X).~n",[J,I]),
-        format(Out,"i~d(X) :- n~d(X).~n",[J,I]),
-        format(Out,"j~d(X) :- n~d(X).~n",[J,I]),
+        format(Out,"'http://example.org/etc#n~d'(X) :- 'http://example.org/etc#n~d'(X).~n",[J,I]),
+        format(Out,"'http://example.org/etc#i~d'(X) :- 'http://example.org/etc#n~d'(X).~n",[J,I]),
+        format(Out,"'http://example.org/etc#j~d'(X) :- 'http://example.org/etc#n~d'(X).~n",[J,I]),
         fail
     ;   true
     ),
     write(Out,'\n'),
     write(Out,'% query implies goal\n'),
-    write(Out,'n1(_ELEMENT) -: goal.\n'),
-    write(Out,'n10(_ELEMENT) -: goal.\n'),
-    write(Out,'n100(_ELEMENT) -: goal.\n'),
-    write(Out,'n1000(_ELEMENT) -: goal.\n'),
-    write(Out,'n10000(_ELEMENT) -: goal.\n'),
+    write(Out,'\'http://example.org/etc#n1\'(_ELEMENT) => goal.\n'),
+    write(Out,'\'http://example.org/etc#n10\'(_ELEMENT) => goal.\n'),
+    write(Out,'\'http://example.org/etc#n100\'(_ELEMENT) => goal.\n'),
+    write(Out,'\'http://example.org/etc#n1000\'(_ELEMENT) => goal.\n'),
     close(Out).
