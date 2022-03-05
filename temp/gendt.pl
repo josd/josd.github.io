affe37c36a4941ed965fef07da9275c19ca3936a
josd <josderoo@gmail.com>
2022-03-05 18:12:23 +0100

    refreshing

--- temp/gendt.pl
+++ temp/gendt.pl
@@ -4,26 +4,26 @@
 :- use_module(library(format)).
 
 run :-
-    open('dt.pl',write,Out),
+    open('dt.n3p',write,Out),
     write(Out,'% Deep taxonomy\n'),
     write(Out,'% See http://ruleml.org/WellnessRules/files/WellnessRulesN3-2009-11-10.pdf\n'),
     write(Out,'\n'),
     write(Out,':- op(1150,xfx,=>).\n'),
+    write(Out,':- op(1145,xfx,<=).\n'),
+    write(Out,'\n'),
+    write(Out,'true <= \'https://idlabresearch.github.io/ns#builtin\'(dynamic(\'http://www.w3.org/1999/02/22-rdf-syntax-ns#type\'/2),true).\n'),
     write(Out,'\n'),
     write(Out,'\'http://www.w3.org/1999/02/22-rdf-syntax-ns#type\'(\'http://example.org/etc#z\',\'http://example.org/etc#N0\').\n'),
     write(Out,'\n'),
     (   between(0,9999,I),
         J is I+1,
-        format(Out,"'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/etc#N~d') :- 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/etc#N~d').~n",[J,I]),
-        format(Out,"'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/etc#I~d') :- 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/etc#N~d').~n",[J,I]),
-        format(Out,"'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/etc#J~d') :- 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/etc#N~d').~n",[J,I]),
+        format(Out,"'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/etc#N~d') <= 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/etc#N~d').~n",[J,I]),
+        format(Out,"'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/etc#I~d') <= 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/etc#N~d').~n",[J,I]),
+        format(Out,"'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/etc#J~d') <= 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/etc#N~d').~n",[J,I]),
         fail
     ;   true
     ),
     write(Out,'\n'),
     write(Out,'% query\n'),
-    write(Out,'\'http://www.w3.org/1999/02/22-rdf-syntax-ns#type\'(_ELEMENT,\'http://example.org/etc#N10\') => yes.\n'),
-    write(Out,'\'http://www.w3.org/1999/02/22-rdf-syntax-ns#type\'(_ELEMENT,\'http://example.org/etc#N100\') => yes.\n'),
-    write(Out,'\'http://www.w3.org/1999/02/22-rdf-syntax-ns#type\'(_ELEMENT,\'http://example.org/etc#N1000\') => yes.\n'),
-    write(Out,'\'http://www.w3.org/1999/02/22-rdf-syntax-ns#type\'(_ELEMENT,\'http://example.org/etc#N10000\') => yes.\n'),
+    write(Out,'\'http://www.w3.org/1999/02/22-rdf-syntax-ns#type\'(_ELEMENT,\'http://example.org/etc#N10000\') => true.\n'),
     close(Out).
