7dcbd3afaf0d6268ed81e119c103e0299aeab7da
josd <josderoo@gmail.com>
2022-05-11 23:29:34 +0200

    refreshing

--- temp/elf/elf.pl
+++
@@ -1,8 +0,0 @@
-:- op(1150,xfx,=>).
-:- op(1175,xfx,<=).
-
-term_expansion((Head <= Body),(Head :- Body)).
-term_expansion(('elf:directive'(Term,-)),(:- Term)).
-
-goal_expansion('elf:control'(Goal,-),Goal).
-goal_expansion('elf:builtin'(Goal,-),Goal).
