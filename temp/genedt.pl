60699b535aaeb8aeb1868667669730d837ada0fd
josd <josderoo@gmail.com>
2022-05-15 12:06:15 +0200

    testing extended deep taxonomy

---
+++ temp/genedt.pl
@@ -0,0 +1,30 @@
+% Generating extended deep taxonomy
+
+:- use_module(library(format)).
+:- use_module(library(between)).
+
+run :-
+    open('edt.pl',write,Out),
+    format(Out,"% Extended deep taxonomy~n",[]),
+    format(Out,"% See http://ruleml.org/WellnessRules/files/WellnessRulesN3-2009-11-10.pdf~n",[]),
+    format(Out,"~n",[]),
+    format(Out,":- dynamic('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'/2).~n",[]),
+    format(Out,"~n",[]),
+    (   between(0,10000,I),
+        format(Out,"'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'('http://example.org/ns#i~d','http://example.org/ns#N0').~n",[I]),
+        fail
+    ;   true
+    ),
+    format(Out,"~n",[]),
+    (   between(0,9999,I),
+        J is I+1,
+        format(Out,"'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/ns#N~d') :- 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/ns#N~d').~n",[J,I]),
+        format(Out,"'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/ns#I~d') :- 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/ns#N~d').~n",[J,I]),
+        format(Out,"'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/ns#J~d') :- 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X,'http://example.org/ns#N~d').~n",[J,I]),
+        fail
+    ;   true
+    ),
+    format(Out,"~n",[]),
+    format(Out,"% query~n",[]),
+    format(Out,"run :- 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'('http://example.org/ns#i10000','http://example.org/ns#N10000'),write(true),nl.~n",[]),
+    close(Out).
