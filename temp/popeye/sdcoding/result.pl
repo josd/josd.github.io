697400b741982b226c31bc1b9c6e2057d3dd00f0
josd <josderoo@gmail.com>
2022-06-29 15:48:57 +0200

    refreshing

--- temp/popeye/sdcoding/result.pl
+++
@@ -1,4 +0,0 @@
-'https://josd.github.io/popeye#sdcoding'(1,1).
-'https://josd.github.io/popeye#sdcoding'(3,3).
-'https://josd.github.io/popeye#sdcoding'(0,0).
-'https://josd.github.io/popeye#sdcoding'(2,2).
