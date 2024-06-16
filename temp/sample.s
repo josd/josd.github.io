16caf3a10d1436de5e8a42dfd76df7451816d461
josd <josderoo@gmail.com>
2024-06-16 19:36:55 +0200

    refreshing

--- temp/sample.s
+++ temp/sample.s
@@ -1,9 +1,9 @@
-flag('quantify', 'https://eyereasoner.github.io/.well-known/genid/18ed0e3b-7e21-4b82-9834-765af2728b65#').
+flag('quantify', 'https://eyereasoner.github.io/.well-known/genid/f5187504-bb2b-4bcf-875a-ca02dfd7b7dd#').
 scope('<file:///home/jdroo/github.com/josd/josd.github.io/temp/sample.ttl>').
 pfx(:, '<urn:example:>').
 :- dynamic('<urn:example:b>'/2).
 '<urn:example:b>'('<urn:example:a>', '<urn:example:c>').
-:- dynamic('<urn:example:e>'/2).
-'<urn:example:e>'('<https://eyereasoner.github.io/.well-known/genid/18ed0e3b-7e21-4b82-9834-765af2728b65#e_d_1>', 6).
+:- dynamic('<https://eyereasoner.github.io/.well-known/genid/f5187504-bb2b-4bcf-875a-ca02dfd7b7dd#e_e_1>'/2).
+'<https://eyereasoner.github.io/.well-known/genid/f5187504-bb2b-4bcf-875a-ca02dfd7b7dd#e_e_1>'('<https://eyereasoner.github.io/.well-known/genid/f5187504-bb2b-4bcf-875a-ca02dfd7b7dd#e_d_1>', 6).
 scount(2).
 end_of_file.
